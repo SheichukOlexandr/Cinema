c23d2171a27d50f5a261b593d8dfaae329e368a5
Шейчук Олександр <sheichuk.oleksandr@chnu.edu.ua>
2025-02-01 19:26:53 +0200

    add script for hide input passwod

--- MVC_Cinema_app/wwwroot/js/authpage.js
+++ MVC_Cinema_app/wwwroot/js/authpage.js
@@ -9,3 +9,27 @@ registerBtn.addEventListener('click', () => {
 loginBtn.addEventListener('click', () => {
     container.classList.remove('active');
 })
+
+document.getElementById('toggleLoginPassword').addEventListener('click', function (e) {
+    const password = document.getElementById('loginPassword');
+    const type = password.getAttribute('type') === 'password' ? 'text' : 'password';
+    password.setAttribute('type', type);
+    this.classList.toggle('bxs-hide');
+    this.classList.toggle('bxs-show');
+});
+
+document.getElementById('toggleRegisterPassword').addEventListener('click', function (e) {
+    const password = document.getElementById('registerPassword');
+    const type = password.getAttribute('type') === 'password' ? 'text' : 'password';
+    password.setAttribute('type', type);
+    this.classList.toggle('bxs-hide');
+    this.classList.toggle('bxs-show');
+});
+
+document.getElementById('toggleConfirmPassword').addEventListener('click', function (e) {
+    const password = document.getElementById('confirmPassword');
+    const type = password.getAttribute('type') === 'password' ? 'text' : 'password';
+    password.setAttribute('type', type);
+    this.classList.toggle('bxs-hide');
+    this.classList.toggle('bxs-show');
+});
