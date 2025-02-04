28ce8bad1735f88a30816e16fb9e48850dbb81d2
Katinuka <9a523ab@mailfence.com>
2025-02-04 14:05:11 +0200

    check the date on update

--- MVC_Cinema_app/Controllers/SessionsController.cs
+++ MVC_Cinema_app/Controllers/SessionsController.cs
@@ -97,6 +97,10 @@ namespace MVC_Cinema_app.Controllers
                 return NotFound();
             }
 
+            if (!await _sessionService.ValidateSesionDate(session))
+            {
+                ModelState.AddModelError("Date", "Сеанс не може відбутися до релізу фільму.");
+            }
             if (ModelState.IsValid)
             {
                 try
