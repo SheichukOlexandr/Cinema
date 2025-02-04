921a5208bfac981c81b2f4f027f7cb53a22cc242
Danylo-Skripka <skrypka.danylo@chnu.edu.ua>
2025-02-04 14:32:02 +0200

    Added checks

--- BusinessLogic/DTOs/SeatDTO.cs
+++ BusinessLogic/DTOs/SeatDTO.cs
@@ -1,14 +1,22 @@
-﻿namespace BusinessLogic.DTOs
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BusinessLogic.DTOs
 {
     public class SeatDTO
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "Ідентифікатор кімнати є обов'язковим.")]
         public int RoomId { get; set; }
+
+        [Required(ErrorMessage = "Номер місця є обов'язковим.")]
+        [Range(1, int.MaxValue, ErrorMessage = "Номер місця повинен бути додатним числом.")]
         public int Number { get; set; }
-        public Decimal ExtraPrice { get; set; }
 
-        // data to map:
+        [Range(0, double.MaxValue, ErrorMessage = "Додаткова вартість повинна бути невід'ємною.")]
+        public decimal ExtraPrice { get; set; }
+
+        // Дані для мапінгу:
         public string? RoomName { get; set; }
-
     }
 }
